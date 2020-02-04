394efd324d05669c47128c0918958deb14f7e8e8
KateRaymundi <kateacristina@outlook.com>
2020-02-04 05:42:52 +0000

    Implement Tail with new array added

--- tail.js
+++ tail.js
@@ -8,9 +8,15 @@ const assertEqual = function(actual, expected) {
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 
-let tail = function (x) {
-  return  x.splice(0, 1)
-}
+let tail = function(arr) {
+  let arrCopy = [...arr];
+  arrCopy.splice(0, 1); // splice runs (no need to create variable)
+  return arrCopy;
+};
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result, ["Lighthouse", "Labs"]); 
+assertEqual(result, ["Lighthouse", "Labs"]);
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
