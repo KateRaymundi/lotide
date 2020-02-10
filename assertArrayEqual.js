83f3b1a105d03bf8cb9220f5aed24c9930a76725
KateRaymundi <kateacristina@outlook.com>
2020-02-10 19:17:19 +0000

    refactored function test

--- assertArrayEqual.js
+++ assertArrayEqual.js
@@ -1,20 +1,4 @@
-const eqArrays = function(arr1, arr2){
-
-  let result;
-  if(arr1.length !== arr2.length){
-    return false
-  }
-  for (i = 0; i < arr1.length; i++){
-    if(arr1[i] === arr2[i]){
-      result = true
-    }
-    else {
-      return false
-    }
-  }
-  return result
-} 
-
+const eqArrays = require ("./eqArrays")
 const assertArrayEqual = function(array1, array2) {
   if (eqArrays(array1, array2)){
     console.log(`\u{2705} \u{2705} \u{2705} Assertion Passed: ${array1} === ${array2}`);
@@ -24,3 +8,5 @@ const assertArrayEqual = function(array1, array2) {
 }
 
 assertArrayEqual([1, 2], [1, 2, 3])
+
+module.exports = assertArrayEqual;
---
+++ test/assertArrayEqualTest.js
@@ -0,0 +1,4 @@
+const assertArrayEqual = require ("../assertArrayEqual")
+
+
+assertArrayEqual([1, 2], [1, 2, 3])
